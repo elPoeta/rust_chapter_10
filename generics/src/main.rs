adc252687deb60bca7b2f7840d97c3f5b2ce142a
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-22 19:38:45 -0300

    implement generic fn

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -24,14 +24,39 @@ fn largest_char(list: &[char]) -> &char {
     largest
 }
 
+fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
+    let mut largest = &list[0];
+
+    for item in list {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
 fn main() {
-    let number_list = vec![34, 50, 25, 100, 65];
+    {
+        let number_list = vec![34, 50, 25, 100, 65];
 
-    let result = largest_i32(&number_list);
-    println!("The largest number is {}", result);
+        let result = largest_i32(&number_list);
+        println!("The largest number is {}", result);
 
-    let char_list = vec!['y', 'm', 'a', 'q'];
+        let char_list = vec!['y', 'm', 'a', 'q'];
 
-    let result = largest_char(&char_list);
-    println!("The largest char is {}", result);
+        let result = largest_char(&char_list);
+        println!("The largest char is {}", result);
+    }
+    {
+        let number_list = vec![34, 50, 25, 100, 65];
+
+        let result = largest_i32(&number_list);
+        println!("GENERIC The largest number is {}", result);
+
+        let char_list = vec!['y', 'm', 'a', 'q'];
+
+        let result = largest_char(&char_list);
+        println!("GENERIC The largest char is {}", result);
+    }
 }
