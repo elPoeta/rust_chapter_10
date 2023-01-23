551ea88069bd63b5d97db68c5b75e4031320d0be
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2023-01-23 07:55:14 -0300

    generi structs - enums and method definitions

--- generics/src/main.rs
+++ generics/src/main.rs
@@ -37,6 +37,7 @@ fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
 }
 
 fn main() {
+    // GENERIC FUNCTIONS
     {
         let number_list = vec![34, 50, 25, 100, 65];
 
@@ -58,5 +59,82 @@ fn main() {
 
         let result = largest_char(&char_list);
         println!("GENERIC The largest char is {}", result);
+    }
+    // GENERIC STRUCTS
+    {
+        struct Point<T> {
+            x: T,
+            y: T,
+        }
+
+        let integer = Point { x: 5, y: 10 };
+        let float = Point { x: 1.0, y: 4.0 };
+        // let wont_work = Point { x: 5, y: 4.0 }; different types
+    }
+    {
+        struct Point<T, U> {
+            x: T,
+            y: U,
+        }
+        let both_integer = Point { x: 5, y: 10 };
+        let both_float = Point { x: 1.0, y: 4.0 };
+        let integer_and_float = Point { x: 5, y: 4.0 };
+    }
+
+    //GENERIC ENUMS
+    {
+        enum Option<T> {
+            Some(T),
+            None,
+        }
+        enum Result<T, E> {
+            Ok(T),
+            Err(E),
+        }
+    }
+
+    // GENERIC METHOD DEFINITIONS
+    {
+        struct Point<T> {
+            x: T,
+            y: T,
+        }
+
+        impl<T> Point<T> {
+            fn x(&self) -> &T {
+                &self.x
+            }
+        }
+
+        impl Point<f32> {
+            fn distance_from_origin(&self) -> f32 {
+                (self.x.powi(2) + self.y.powi(2)).sqrt()
+            }
+        }
+
+        let p = Point { x: 5, y: 10 };
+
+        println!("p.x = {}", p.x());
+    }
+    {
+        struct Point<X1, Y1> {
+            x: X1,
+            y: Y1,
+        }
+
+        impl<X1, Y1> Point<X1, Y1> {
+            fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+                Point {
+                    x: self.x,
+                    y: other.y,
+                }
+            }
+        }
+        let p1 = Point { x: 5, y: 10.4 };
+        let p2 = Point { x: "Hello", y: 'c' };
+
+        let p3 = p1.mixup(p2);
+
+        println!("p3.x = {}, p3.y = {}", p3.x, p3.y);
     }
 }
